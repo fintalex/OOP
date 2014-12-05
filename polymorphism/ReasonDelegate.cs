8cb10136d63416e90eaa4911e1228ae1a855dc97
fintalex <fintalex@mail>
2014-12-05 11:35:34 +0300

    Update ReasonDelegate.cs

--- polymorphism/ReasonDelegate.cs
+++ polymorphism/ReasonDelegate.cs
@@ -17,6 +17,11 @@ namespace FuncAction
 			Console.WriteLine(calc.PerformOperation("+", 23, 3));
 			Console.WriteLine(calc.PerformNewOperation("+", 23, 3));
 
+			// использования операции добавления операции )))
+			calc.DefineOperation("mod", (x,y)=>x%y);
+
+			Console.WriteLine(calc.PerformNewOperation("mod", 23, 3));
+
 			Console.ReadLine();
 		}
 
@@ -35,23 +40,51 @@ namespace FuncAction
 		}
 
 		// но надо оптимизировать
-		private double DoDevision(double x, double y) { return x / y; }
-		private double DoMultiplication(double x, double y) { return x * y; }
-		private double DoSubtraction(double x, double y) { return x - y; }
-		private double DoAddition(double x, double y) { return x + y; }
 		// и желательно избавиться от swithch
+		 // Мы вынесли определение операций из кода в данные — из свитча в словарь.
 		private delegate double OperationDelegate(double x, double y); // делегат для выполнения операции с двумя числами (бинарный)
-		private Dictionary<string, OperationDelegate> operations; // словарь в котором будем хранить пары ( операция - кусок кода (функции) для выполнения)
+		private Dictionary<string, Func<double, double, double>> operations; // словарь в котором будем хранить пары ( операция - кусок кода (функции) для выполнения)
 
+		// но этот конструктор и функции слишком громоздкие и их можно заменить 
+		//private double DoDevision(double x, double y) { return x / y; }
+		//private double DoMultiplication(double x, double y) { return x * y; }
+		//private double DoSubtraction(double x, double y) { return x - y; }
+		//private double DoAddition(double x, double y) { return x + y; }
+		//public Calculator()
+		//{
+		//	operations = 
+		//		new Dictionary<string,OperationDelegate>
+		//		{
+		//			{"+", this.DoAddition},
+		//			{"-", this.DoSubtraction},
+		//			{"*", this.DoMultiplication},
+		//			{"/", this.DoDevision}
+		//		};
+		//}
+
+		// и мы можем избавить от всех этих доп функций и написать при помощь анонимных методов:
+		//public Calculator()
+		//{
+		//	operations =
+		//		new Dictionary<string, OperationDelegate>
+		//		{
+		//			{"+", delegate(double x, double y) {return x + y;}},
+		//			{"-", delegate(double x, double y) {return x - y;}},
+		//			{"*", delegate(double x, double y) {return x * y;}},
+		//			{"/", delegate(double x, double y) {return x / y;}}
+		//		};
+		//}
+
+		// но выше тоже много лабуды, поэтому переписать все это можно через лямбда выражения
 		public Calculator()
 		{
-			operations = 
-				new Dictionary<string,OperationDelegate>
+			operations =
+				new Dictionary<string, Func<double, double, double>>
 				{
-					{"+", this.DoAddition},
-					{"-", this.DoSubtraction},
-					{"*", this.DoMultiplication},
-					{"/", this.DoDevision}
+					{"+", (x,y) => x + y },
+					{"-", (x,y) => x - y },
+					{"*", (x,y) => x * y },
+					{"/", (x,y) => x / y }
 				};
 		}
 
@@ -63,5 +96,14 @@ namespace FuncAction
 		}
 
 
+
+		// НОООО, теперь мы можем изящно в коде написать функцию по добавлению новых видов операци программно
+		public void DefineOperation(string op, Func<double, double, double> newOper)
+		{ 
+			if (operations.ContainsKey(op))
+				throw new ArgumentException(string.Format("Operation {0} already exists", op), "op");
+
+			operations.Add(op, newOper);
+		}
 	}
 }
