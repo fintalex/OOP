aa0f0c18cc39a0843523f8c70c846ae53c0f646d
fintalex <fintalex@mail.ru>
2015-05-22 11:53:50 +0300

    Sanderson First app
    Возможно не здесь его надо коммитить

---
+++ SandersonFirstApp/SandersonFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SandersonFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+			int hour = DateTime.Now.Hour;
+			ViewData["greeting"] = (hour < 12 ? "good morning" : "Good afternoon");
+
+			return View();
+        }
+
+		[AcceptVerbs(HttpVerbs.Get)]
+		public ViewResult RSVPForm()
+		{
+			return View();
+		}
+		[AcceptVerbs(HttpVerbs.Post)]
+		public ViewResult RSVPForm(GuestResponse guestResponse)
+		{
+			if (ModelState.IsValid)
+			{
+				// todo: send by email
+				return View("Thanks", guestResponse);
+			}
+			else
+			{ 
+				// ошибка проверки достоверности, отображаем форму данных заново
+				return View(guestResponse);
+			}
+		}
+    }
+}
---
+++ SandersonFirstApp/SandersonFirstApp/Models/GuestResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Models
+{
+	public class GuestResponse : IDataErrorInfo
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public bool? WillAttend { get; set; }
+
+
+
+		public string Error	{get { return null; }} // in our example dont uses
+
+		public string this[string columnName]
+		{
+			get { 
+				if (columnName =="Name" && string.IsNullOrEmpty(Name))
+					return "Please enter your name"; // не было введено имя
+
+				if (columnName == "Email" && !Regex.IsMatch(Email, ".+\\@.+\\..+"))
+					return "Please enter a valid email address"; // введен неверный email
+
+				if (columnName == "Phone" && string.IsNullOrEmpty(Phone))
+					return "Please enter your phone number"; // не был введен телефон
+
+				if (columnName == "WillAttend" && !WillAttend.HasValue)
+					return "Please specify wheter you'll attend"; // не было указано планирует ли он посетить
+
+				return null;
+			}
+		}
+	}
+}
