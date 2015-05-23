90ddb62072253ca592e8a30e5e0b8ac3279deb86
fintalex <fintalex@mail>
2015-05-23 20:34:55 +0300

    Add Entity Framework

---
+++ DBDemo/DBDemo/CustomerProxy.cs
@@ -0,0 +1,46 @@
+﻿using DBDemo.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBDemo
+{
+    public class CustomerProxy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<CustomerProxy> GetCustomers()
+        {
+            using (IDbConnection connection = new SqlConnection(Settings.Default.DbConnect))
+            {
+                IDbCommand command = new SqlCommand("Select * from t_customer");
+
+                command.Connection = connection;
+
+                connection.Open();
+
+                IDataReader reader = command.ExecuteReader();
+
+                List<CustomerProxy> customers = new List<CustomerProxy>();
+
+                while (reader.Read())
+                {
+                    CustomerProxy customer = new CustomerProxy();
+                    customer.Id = reader.GetInt32(0);
+                    customer.Name = reader.GetString(1);
+
+                    customers.Add(customer);
+                }
+
+                return customers;
+            }
+
+            
+        }
+    }
+}
--- DBDemo/DBDemo/Program.cs
+++ DBDemo/DBDemo/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DBDemo.Properties;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,37 @@ namespace DBDemo
     {
         static void Main(string[] args)
         {
+            // это все низкоуровневая работа с базой данных
+            // Минусы:
+            // 1- в С# коде используем sql запрос
+            // 2- приходится заботится об открытии соединения
+            // 3- работаем с нетипизированным набором данных (мы сами должны знать на какой позиции что должно быть) (раньше так и делали)
+
+            //using (IDbConnection connection = new SqlConnection(Settings.Default.DbConnect))
+            //{
+            //    IDbCommand command = new SqlCommand("Select * from t_customer");
+
+            //    command.Connection = connection;
+
+            //    connection.Open();
+
+            //    IDataReader reader = command.ExecuteReader();
+
+            //    while (reader.Read())
+            //    {
+            //        Console.WriteLine("Идентификатор: {0}\Имя:{1}", reader.GetInt32(0), reader.GetString(1));
+            //    }
+            //}
+
+
+            //var customers = CustomerProxy.GetCustomers();
+
+            //foreach (var cust in customers)
+            //{
+            //    Console.WriteLine("Идентификатор: {0}\tИмя:{1}", cust.Id, cust.Name);
+            //}
+
+            //Console.ReadLine();
         }
     }
 }
