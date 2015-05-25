3dccdc3c254da71c2ae8ff42643b1c81a3815fbd
fintalex <fintalex@mail>
2015-05-25 12:12:38 +0300

    SportStore
    Added controller, view and shared

---
+++ SportStore/WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel.Abstract;
+using DomainModel.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductsRepository productsRepository;
+
+        public ProductsController()
+        {
+            // это временно пока не будет готова инфраструктура
+            productsRepository = new FakeProductsRepository();
+        }
+
+        public ViewResult List()
+        {
+            return View(productsRepository.Products.ToList());
+        }
+    }
+}
