e1520f3e2c28653d1925428b2a21e39fa3f55252
fintalex <fintalex@mail>
2015-06-07 10:27:48 +0300

    config odata

---
+++ ProductService/ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+using ProductService.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // new code: 
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+            // This code does two things:
+
+            // 1 - Creates an Entity Data Model (EDM).
+            // 2 - Adds a route.
+
+
+
+
+            //// Web API configuration and services
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
